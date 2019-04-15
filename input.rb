6939802377d984d3c50e7e45313880b5257844e4
vvbv <jeison.cardona@correounivalle.edu.co>
2019-04-15 14:46:32 -0500

    test

--- input.rb
+++ input.rb
@@ -1,4 +1,8 @@
-puts (3*[1,2,3]);
-puts (3*[1,"ne",3]);
-
+puts (1+(2+(3+4)));
+puts ((1+2)+(3+4));
+puts ((1+(2+3))+4);
+puts ("Hola"+" mundo");
+puts (["hola"]+["mundo"]);
+puts ("hola"*2);
+puts ([1,2]*2);
 
