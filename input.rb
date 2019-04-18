cbda6459506508c8d3580bb5b08de74902c33207
vvbv <jeison.cardona@correounivalle.edu.co>
2019-04-18 11:35:09 -0500

    test

--- input.rb
+++ input.rb
@@ -1,26 +1,4 @@
-for x in (1..10) do
-	puts x;
-end
+declare a;
+a=[[1,2],3];
 
-def enIntervalo(min,max,value)
-	declare to_return;
-	to_return = false;
-	if (min < value) then
-		if (value < max) then
-			to_return = true;
-		end
-	end
-	return to_return;
-end
-
-declare min, max, value, w;
-
-w = enIntervalo(1, 10, 5);
-if w then
-	puts "En el intervalo";
-end
-
-w = enIntervalo(1, 10, 11);
-unless w then
-	puts "Fuera del intervalo";
-end
+puts (a[0]);
